82bae89c09f997e2326b2f8bf6f3d3136d905aa0
otavionvs <92130086+otavionvs@users.noreply.github.com>
2024-06-26 22:06:35 -0300

    Empresa

--- src/services/Company/CompanyService.ts
+++ src/services/Company/CompanyService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { CompanyModel } from "interfaces/Company/ComanyInterface";
 
-export class CompaniesService {
+export class CompanyService {
   private API = "http://localhost:8085/acessi";
 
   async CreateCompany(company: CompanyModel) {
